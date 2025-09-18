9cc2183004ef9f92c0a3e9116dc6acdba73dd7cf
João Lucca <joao@portovaleconsorcios.com.br>
2025-09-18 12:08:56 +0000

    Este botão está diferente dos demais, arredonde a borda para ficar em co

--- src/components/sections/table-section.tsx
+++ src/components/sections/table-section.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Image from 'next/image';
@@ -54,7 +53,7 @@ export default function TableSection() {
         <Button
           onClick={scrollToForm}
           size="lg"
-          className="mt-8 bg-primary text-primary-foreground hover:bg-primary/90 hover:shadow-lg transform hover:-translate-y-1 transition-all duration-300 w-full sm:w-auto uppercase font-bold"
+          className="mt-8 bg-primary text-primary-foreground hover:bg-primary/90 hover:shadow-lg transform hover:-translate-y-1 transition-all duration-300 w-full sm:w-auto uppercase font-bold rounded-full"
         >
           Fazer uma simulação!
         </Button>
